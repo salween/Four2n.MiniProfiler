e4d7934240431cc4595577a51edf6c0f267138ca
randompete <none@none>
2011-12-10 21:52:59 +0000

    Remove dispose error

--- src/Four2n.MiniProfiler/Services/ProfilerService.cs
+++ src/Four2n.MiniProfiler/Services/ProfilerService.cs
@@ -8,9 +8,18 @@ namespace Four2n.Orchard.MiniProfiler.Services {
 
     using MvcMiniProfiler;
 
-    public class ProfilerService : IProfilerService, IDisposable  {
+    public class ProfilerService : IProfilerService  {
 
-        private readonly MiniProfiler _profiler;
+        private MiniProfiler _profiler;
+        protected MiniProfiler profiler {
+            get {
+                // The event bus starts in a different scope where there's no MiniProfiler.Current, set it now
+                if (_profiler == null) {
+                    _profiler = MiniProfiler.Current;
+                }
+                return _profiler;
+            }
+        }
         private readonly ConcurrentDictionary<string, ConcurrentStack<IDisposable>> _steps = new ConcurrentDictionary<string, ConcurrentStack<IDisposable>>();
 
         public ProfilerService() {
@@ -19,30 +28,32 @@ namespace Four2n.Orchard.MiniProfiler.Services {
 
         public void StepStart(string key, string message, bool isVerbose = false)
         {
-            var step = _profiler.Step(message, isVerbose?ProfileLevel.Verbose:ProfileLevel.Info);
+            if (profiler == null) return;
             var stack = _steps.GetOrAdd(key, (k) => {
                 return new ConcurrentStack<IDisposable>();
             });
+            var step = profiler.Step(message, isVerbose ? ProfileLevel.Verbose : ProfileLevel.Info);
             stack.Push(step);
         }
 
         public void StepStop(string key)
         {
+            if (profiler == null) return;
             IDisposable step;
             if (_steps[key].TryPop(out step)) {
                 step.Dispose();
             }
         }
 
-        public void Dispose() {
+        public void StopAll() {
 
             // Dispose any orphaned steps
             foreach (var stack in _steps.Values) {
 
-                foreach (var step in stack) {
+                IDisposable step;
+                while (stack.TryPop(out step)) {
                     step.Dispose();
                 }
-
             }
 
         }
